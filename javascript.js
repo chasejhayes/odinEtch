dd1b696335d49784a7650b4352b6325ccd0222d4
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2026-02-27 17:13:01 -0500

    Add player button choice logic

--- javascript.js
+++ javascript.js
@@ -1,33 +1,66 @@
 const container = document.querySelector("#container");
 container.style.display = "flex";
 
-function createGrid() {
-    for (let i = 0; i < 16; i++) {
-        line = document.createElement("div");
-        container.appendChild(line);
-        for (let j = 0; j < 16; j++) {
-            div = document.createElement("div");
-            div.classList.add("colorBox")
-            div.style.cssText = "background-color: red; height: 10px; width: 10px; margin: 2px;";
-            line.appendChild(div)
+const promptButton = document.createElement("button");
+promptButton.style.cssText = "height: 25px; width: auto; background-color: gray; color: white";
+promptButton.textContent = "Press to Play";
+container.appendChild(promptButton);
+
+let numberOfDivs = 0;
+
+promptButton.addEventListener("click", () => {
+    numberOfDivs = Number(prompt("Enter number:"));
+    function playEtch(numberOfDivs) {
+        function createGrid() {
+            for (let i = 0; i < numberOfDivs; i++) {
+                let line = document.createElement("div");
+                container.appendChild(line);
+                for (let j = 0; j < numberOfDivs; j++) {
+                    let div = document.createElement("div");
+                    div.classList.add("colorBox")
+                    div.style.cssText = "background-color: red; height: 10px; width: 10px; margin: 2px;";
+                    line.appendChild(div)
+                }
+            }
         }
+        createGrid();
+        const colorBox = document.querySelectorAll(".colorBox");
+
+        colorBox.forEach(box => {
+            box.addEventListener("mouseover", () => {
+                box.style.backgroundColor = "aqua";
+            })
+        })
     }
-}
+    playEtch(numberOfDivs);
+});
 
-createGrid();
 
-function changeColor(test){
-    test.style.color = "peach"
+// function playEtch(numberOfDivs) {
+//     function createGrid() {
+//         for (let i = 0; i < numberOfDivs; i++) {
+//             line = document.createElement("div");
+//             container.appendChild(line);
+//             for (let j = 0; j < numberOfDivs; j++) {
+//                 div = document.createElement("div");
+//                 div.classList.add("colorBox")
+//                 div.style.cssText = "background-color: red; height: 10px; width: 10px; margin: 2px;";
+//                 line.appendChild(div)
+//             }
+//         }
+//     }
 
-}
+//     createGrid();
 
-const colorBox = document.querySelectorAll(".colorBox");
+//     const colorBox = document.querySelectorAll(".colorBox");
 
-colorBox.forEach(box => {
-    box.addEventListener("mouseover", ()=> {
-        box.style.backgroundColor = "aqua";
-    })
-})
+//     colorBox.forEach(box => {
+//         box.addEventListener("mouseover", () => {
+//             box.style.backgroundColor = "aqua";
+//         })
+//     })
+// }
 
+// playEtch(playerSelection);
 
 
