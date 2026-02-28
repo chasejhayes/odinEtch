ffc273ec1111adcd0e4e4a08f37144f2f3c52c4d
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2026-02-28 11:11:04 -0500

    Add logic to keep the canvas the same size

--- javascript.js
+++ javascript.js
@@ -7,31 +7,44 @@ button.appendChild(promptButton);
 
 const container = document.querySelector("#container");
 container.style.display = "flex";
-container.style.maxWidth = "50px";
-container.style.maxHeight = "500px";
-
 
 
 let numberOfDivs = 0;
 
 promptButton.addEventListener("click", () => {
+    // assigns player entered number to variable "numberOfDivs"
     numberOfDivs = Number(prompt("Enter number:"));
+    // creates function to play game
     function playEtch(numberOfDivs) {
+        // creates function to create grid
         function createGrid() {
+            // clears previous grid
             document.getElementById("container").innerHTML = "";
+            // stop loop if number higher than 50 is entered
             if (numberOfDivs > 50) {
                 return alert("Must be 50 or less")
             } else {
+                // runs a loop that's based on player entered number
                 for (let i = 0; i < numberOfDivs; i++) {
+                    // every loop creates a group of empty divs
                     let line = document.createElement("div");
+                    line.style.display = "flex";
+                    line.style.flexDirection = "column";
+                    line.style.flex = "1";
+
+                    // puts that div onto the container
                     container.appendChild(line);
+                    // then runs another loop that fills empty divs with colored box divs
                     for (let j = 0; j < numberOfDivs; j++) {
                         let div = document.createElement("div");
                         div.classList.add("colorBox")
-                        div.style.cssText = "background-color: red; height: 10px; width: 10px; margin: 2px;";
+                        div.style.cssText = "background-color: red; margin: 1px";
+                        div.style.flex = "1"
                         line.appendChild(div)
                     }
                 }
+                // what I need to do:
+                // 
             }
 
         }
@@ -49,6 +62,5 @@ promptButton.addEventListener("click", () => {
 
 
 // set a standard total space for the canvas 
-// set a limit for amount of squares the user can imput 
 
 
