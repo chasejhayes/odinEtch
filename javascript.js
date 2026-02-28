669f9efc22b225acd418f8b2c7b0c0816bbfa36f
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2026-02-28 10:06:25 -0500

    Clear canvas every time the button is pushed

--- javascript.js
+++ javascript.js
@@ -1,10 +1,16 @@
-const container = document.querySelector("#container");
-container.style.display = "flex";
+const button = document.querySelector("#button");
 
 const promptButton = document.createElement("button");
 promptButton.style.cssText = "height: 25px; width: auto; background-color: gray; color: white";
 promptButton.textContent = "Press to Play";
-container.appendChild(promptButton);
+button.appendChild(promptButton);
+
+const container = document.querySelector("#container");
+container.style.display = "flex";
+container.style.maxWidth = "50px";
+container.style.maxHeight = "500px";
+
+
 
 let numberOfDivs = 0;
 
@@ -12,6 +18,7 @@ promptButton.addEventListener("click", () => {
     numberOfDivs = Number(prompt("Enter number:"));
     function playEtch(numberOfDivs) {
         function createGrid() {
+            document.getElementById("container").innerHTML = "";
             for (let i = 0; i < numberOfDivs; i++) {
                 let line = document.createElement("div");
                 container.appendChild(line);
@@ -35,32 +42,8 @@ promptButton.addEventListener("click", () => {
     playEtch(numberOfDivs);
 });
 
-
-// function playEtch(numberOfDivs) {
-//     function createGrid() {
-//         for (let i = 0; i < numberOfDivs; i++) {
-//             line = document.createElement("div");
-//             container.appendChild(line);
-//             for (let j = 0; j < numberOfDivs; j++) {
-//                 div = document.createElement("div");
-//                 div.classList.add("colorBox")
-//                 div.style.cssText = "background-color: red; height: 10px; width: 10px; margin: 2px;";
-//                 line.appendChild(div)
-//             }
-//         }
-//     }
-
-//     createGrid();
-
-//     const colorBox = document.querySelectorAll(".colorBox");
-
-//     colorBox.forEach(box => {
-//         box.addEventListener("mouseover", () => {
-//             box.style.backgroundColor = "aqua";
-//         })
-//     })
-// }
-
-// playEtch(playerSelection);
+// clear canvas every time the button is pressed
+// set a standard total space for the canvas 
+// set a limit for amount of squares the user can imput 
 
 
